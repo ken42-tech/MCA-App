32df97aafac88aac4ff951e908fc45a9624ceee7
judson-ts <155189292+judson-ts@users.noreply.github.com>
2025-05-16 16:24:06 +0530

    `Removed unnecessary div wrapper and simplified JSX structure in Home component`

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,13 +1,12 @@
-import React from "react";
 import HomeBanner from "./Components/HomeBanner";
 import Matches from "./Components/Matches";
 
 const Home = () => {
   return (
-    <div className="bg-black">
+    <>
       <HomeBanner />
       <Matches />
-    </div>
+    </>
   );
 };
 
